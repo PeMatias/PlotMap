bd922dc8b83817a325b8e8b8e30a70103c22913e
Pedro Matias <pvsm@icomp.ufam.edu.br>
2019-04-28 20:13:53 -0400

    Projeto Atualizado, falta so o Map e perfumaria

--- app/src/main/java/com/example/plotmap/model/Continente.java
+++ app/src/main/java/com/example/plotmap/model/Continente.java
@@ -1,6 +1,20 @@
 package com.example.plotmap.model;
 
+import java.util.List;
+
 public class Continente
 {
-    
+    private List<Pais> continente;
+
+    public Continente(List<Pais> continente) {
+        this.continente = continente;
+    }
+
+    public List<Pais> getContinente() {
+        return continente;
+    }
+
+    public void setContinente(List<Pais> continente) {
+        this.continente = continente;
+    }
 }
